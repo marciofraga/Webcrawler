00ab1a6e8949664068365c79d747c436c2600c21
marcio.fraga@sippulse.com <marcior.mfraga@gmail.com>
2022-08-28 11:24:41 -0300

    first commit

---
+++ src/test/java/com/overmind/crawler/webcrawler/WebcrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.overmind.crawler.webcrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebcrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
